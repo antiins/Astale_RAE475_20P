05c8d032f3abfad12d71ca167dc5e17f68f5c806
antiins <antashtale@gmail.com>
2020-07-16 12:12:16 +0300

    TELNET klienta un servera kodi

---
+++ telnet/telnetserver/server.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+import time
+import socket
+import sys
+
+print("\nSVeicinaati\n")
+print("Initialising....\n")
+time.sleep(1)
+
+s = socket.socket()
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
+port = 1234
+s.bind((host, port))
+print(host, "(", ip, ")\n")
+name = input(str("ievadiet savu vardu: "))
+           
+s.listen(1)
+print("\nSagaidiet savienojumu...\n")
+conn, addr = s.accept()
+print("Sanemts savienojums no ", addr[0], "(", addr[1], ")\n")
+
+s_name = conn.recv(1024)
+s_name = s_name.decode()
+print(s_name, "has connected to the chat room\nEnter [e] to exit chat room\n")
+conn.send(name.encode())
+
+while True:
+    message = input(str("Me : "))
+    if message == "[e]":
+        message = "Left chat room!"
+        conn.send(message.encode())
+        print("\n")
+        break
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
